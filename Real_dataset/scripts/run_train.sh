d5419e9ea4fb6c86fa36fa432a860e47e0fc154c
Akhila Yerukola <yerukolaakhila@gmail.com>
2019-08-14 16:21:55 -0700

    Switching branches

--- Real_dataset/scripts/run_train.sh
+++ Real_dataset/scripts/run_train.sh
@@ -2,9 +2,9 @@
 python train.py \
 --seq_len 17 \
 --max_seq_len 17 \
---save save_original_17_flow_true \
+--save flow_len17_dis10_gen150_adv350 \
 --disc_pre_epoch 10 \
 --gen_pre_epoch 150 \
---adversarial_epoch 2000 \
+--adversarial_epoch 350 \
 --lbda 1.0
 
